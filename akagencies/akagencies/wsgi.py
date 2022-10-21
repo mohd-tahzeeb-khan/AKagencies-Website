88fb52698093b6861c2001f5addbde50a4d9b4a6
Mohd Tahzeeb Khan <78680463+Tahzeeb-web-py@users.noreply.github.com>
2022-10-22 01:30:29 +0530

    Backend Project Created/ Django

---
+++ akagencies/akagencies/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for akagencies project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'akagencies.settings')
+
+application = get_wsgi_application()
