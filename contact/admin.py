389f32e5d2216fe6c46330b3c359178f63865e7c
Mohd Tahzeeb Khan <78680463+Tahzeeb-web-py@users.noreply.github.com>
2022-10-23 20:42:26 +0530

    contact details updated in admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from contact.models import contactus
+from contact.models import contactus, contact_details
 
 # Register your models here.
 class admin_contactus(admin.ModelAdmin):
     list=('name', 'email', 'mobile', 'message')
 admin.site.register(contactus, admin_contactus)
+class admin_contactusdetails(admin.ModelAdmin):
+    list=('Phone', 'Alternate_Phone_no', 'Instagram', 'Facebook', 'Twitter', 'Youtube', 'EmailId')
+admin.site.register(contact_details, admin_contactusdetails)
--- contact/models.py
+++ contact/models.py
@@ -8,3 +8,13 @@ class contactus(models.Model):
     email=models.EmailField(max_length=80)
     mobile=models.CharField(max_length=15)
     message=models.CharField(max_length=1000)
+class contact_details(models.Model):
+    Phone=models.CharField(max_length=11)
+    Alternate_Phone_no=models.CharField(max_length=11)
+    Instagram=models.URLField(max_length=250)
+    Facebook=models.URLField(max_length=250)
+    Twitter=models.URLField(max_length=250)
+    Youtube=models.URLField(max_length=250)
+    EmailId=models.EmailField(max_length=100)
+
+
